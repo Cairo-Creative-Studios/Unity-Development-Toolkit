4729dd8577797aff71e06b1efdf53a8de7669087
thechayed <thechayed@gmail.com>
2023-03-05 02:19:57 -0500

    Removed all Reflection based Input features

--- Runtime/Modules/Controllables/Utils/Components/ControllableComponent.cs
+++ Runtime/Modules/Controllables/Utils/Components/ControllableComponent.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
 using NaughtyAttributes;
-using UDT.Attributes;
-using UDT.Reflection;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -13,41 +10,17 @@ namespace UDT.Core.Controllables
     /// Reflection is used to call Methods within classes that implement this interface, based on the name of the input
     /// that is given to it from a Controller.
     /// </summary>
-    public class ControllableComponent : StandardComponent, ISerializationCallbackReceiver
+    public class ControllableComponent : StandardComponent
     {
         public Byte inputByte;
         public bool isPossessed = false;
         public Controller Controller;
-
-        [AllowNesting]
-        [Tooltip("Pairs of input names and method names to call when the input is triggered")]
-        public InputMethodLinker InputsToMethodsMap;
-
-        public override void OnReset()
+        public virtual void OnInputAction(InputAction.CallbackContext context)
         {
-            base.OnReset();
-            InputsToMethodsMap = new InputMethodLinker(this);
-        }
-
-        public override void OnInstantiate()
-        {
-            base.OnInstantiate();
-            InputsToMethodsMap = new InputMethodLinker(this);
-        }
-
-        public void OnInputAction(InputAction.CallbackContext context)
-        {
-            this.CallMethod(InputsToMethodsMap[context.action.name], new object[]{context});
         }
 
         public void OnInputAction(SerializedInput input)
         { 
-            if(input.inputType == SerializedInput.InputType.Button)
-                this.CallMethod(InputsToMethodsMap[input.action], new object[]{input.boolValue});   
-            else if(input.inputType == SerializedInput.InputType.Axis)
-                this.CallMethod(InputsToMethodsMap[input.action], new object[]{input.floatValue});  
-            else if(input.inputType == SerializedInput.InputType.Vector2 && input.vector2Value != Vector2.zero)
-                this.CallMethod(InputsToMethodsMap[input.action], new object[]{input.vector2Value});  
         }
 
         public bool Possess(Controller controller)
@@ -64,16 +37,6 @@ namespace UDT.Core.Controllables
         {
             Controller = null;
             isPossessed = false;
-        }
-
-        public void OnBeforeSerialize()
-        {
-            InputsToMethodsMap.OnBeforeSerialize(this);
-        }
-
-        public void OnAfterDeserialize()
-        {
-            InputsToMethodsMap.OnAfterDeserialize(this);
         }
     }
     
@@ -113,128 +76,6 @@ namespace UDT.Core.Controllables
         public override void OnReset()
         {
             GenerateData();
-        }
-    }
-    
-    /// <summary>
-    /// A struct that holds a list of links between input names and method names.
-    /// </summary>
-    [Serializable]
-    public struct InputMethodLinker
-    {
-        [AllowNesting]
-        public List<Link> links;
-        public object component;
-
-        public string this[string inputName]
-        {
-            get
-            {
-                return links.Find(link => link.inputName == inputName).methodName;
-            }
-        }
-
-        public InputMethodLinker(object component)
-        {
-            this.component = component;
-            
-            links = new List<Link>();
-            
-            var customAttributes = this.component.GetType().GetCustomMethodAttributes<InputMethod>();
-            foreach (var inputMethod in customAttributes)
-            {
-                links.Add(new Link(component, "", inputMethod.name));
-            }
-        }
-
-
-        public void OnBeforeSerialize(object component)
-        {
-            this.component = component;
-            
-            if(links == null)
-                Initialize();
-        }
-
-        public void OnAfterDeserialize(object component)
-        {
-            this.component = component;
-            
-            if(links == null)
-                Initialize();
-        }
-        
-        private void Initialize()
-        {
-            links = new List<Link>();
-            
-            var customAttributes = component.GetType().GetCustomMethodAttributes<InputMethod>();
-            foreach (var inputMethod in customAttributes)
-            {
-                links.Add(new Link(component, "", inputMethod.name, inputMethod.inputType));
-            }
-        }
-
-        [Serializable]
-        public struct Link
-        {
-            public enum InputType
-            {
-                Button,
-                Axis,
-                Vector2
-            }
-            [HideInInspector]
-            public InputType inputType;
-            
-            [ReadOnly] public string methodName;
-            public string inputName;
-            
-            public Link(object component, string inputName, string methodName, InputType inputType = InputType.Button)
-            {
-                this.inputName = inputName;
-                this.methodName = methodName;
-                this.inputType = inputType;
-                
-                if (this.inputType == InputType.Button)
-                {
-                    button = default;
-                }
-                else
-                {
-                    button = new ButtonController(component, inputName);
-                }
-                
-                button = default;
-            }
-
-            [Serializable]
-            public struct ButtonController
-            {
-                private object component;
-                private string inputName;
-                
-                [Button("Simulate Press")]
-                public void SimulatePress()
-                {
-                    var controllableComponent = (ControllableComponent)component;
-                    controllableComponent.CallMethod(inputName);
-                }
-                
-                public ButtonController(object component, string inputName)
-                {
-                    this.component = component;
-                    this.inputName = inputName;
-                }
-            }
-
-            [ShowIf("IsButton")]
-            public ButtonController button;
-            
-            public bool IsButton()
-            {
-                return inputType == InputType.Button;
-            }
         }
     }
 }
