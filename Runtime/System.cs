815b38298da11060585bce422c1a4032c9ad1bfe
thechayed <thechayed@gmail.com>
2023-03-21 02:07:34 -0400

    Made GetComponents static

--- Runtime/System.cs
+++ Runtime/System.cs
@@ -167,7 +167,7 @@ namespace UDT.Core
         /// </summary>
         /// <typeparam name="TComponent"></typeparam>
         /// <returns></returns>
-        public new StandardComponent[] GetComponents<TComponent>() where TComponent : StandardComponent
+        public static new StandardComponent[] GetComponents<TComponent>() where TComponent : StandardComponent
         {
             return Instance.Components.FindAll(x => x is TComponent).ToArray();
         }
