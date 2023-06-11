c5992c5c5a50f9f4f77283dbb6639b4d1b0850ef
thechayed <thechayed@gmail.com>
2023-06-11 17:38:44 -0400

    Attempt 3 of fixing the start up crash

--- Runtime/Modules/Core/CoreModule.cs
+++ Runtime/Modules/Core/CoreModule.cs
@@ -23,7 +23,7 @@ namespace UDT.Core
             //Create the Runtime Types list and add all the Runtime Types to it that exist in the current project
             List<Type> runtimeTypes = new List<Type>();
             runtimeTypes.AddRange(Type.GetType("UDT.Core.Runtime`1").GetInheritedTypes()); 
-            //runtimeTypes.AddRange(Type.GetType("UDT.Core.Runtime`2").GetInheritedTypes());
+            runtimeTypes.AddRange(Type.GetType("UDT.Core.Runtime`2").GetInheritedTypes());
 
             foreach (var type in runtimeTypes)
             {
